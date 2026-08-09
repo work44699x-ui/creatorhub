d997fec524fbff370ce0dd1e97382923ea07c467
work44699x-ui <work44699x@gmail.com>
2026-08-09 23:53:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+window.onerror = function (msg, url, line, col, error) {
+  document.getElementById("root").innerHTML =
+    "<pre style='color:red;padding:20px;white-space:pre-wrap;'>" +
+    "ERROR: " + msg + "\nLine: " + line + "\n" + (error && error.stack ? error.stack : "") +
+    "</pre>";
+};
+window.onunhandledrejection = function (event) {
+  document.getElementById("root").innerHTML =
+    "<pre style='color:red;padding:20px;white-space:pre-wrap;'>" +
+    "PROMISE ERROR: " + (event.reason && event.reason.message ? event.reason.message : event.reason) +
+    "</pre>";
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
